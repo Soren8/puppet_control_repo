1a9d32246a2d7347188c2b3166fc3f7cbe9a681b
Soren8 <chris.wall8@gmail.com>
2020-05-06 18:49:56 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,10 +12,10 @@ node 'master.puppet.vm' {
     ensure => file,
     owner => 'root',
     content => '127.0.0.1	master.puppet.vm	master
-                127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
-                ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-                172.18.0.2	web.puppet.vm	web
-                172.18.0.3	db.puppet.vm	db'
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
+172.18.0.2	web.puppet.vm	web
+172.18.0.3	db.puppet.vm	db'
   }
 }
 
