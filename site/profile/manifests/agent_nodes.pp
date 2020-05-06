31a9cc8833d082884d69130d430ffd287e471b5d
Soren8 <chris.wall8@gmail.com>
2020-05-06 19:06:14 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,14 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  host {'web.puppet.vm':
+    ensure       => present,
+    ip           => '172.18.0.2',
+    host_aliases => 'web',
+  }
+  host {'db.puppet.vm':
+    ensure       => present,
+    ip           => '172.18.0.3',
+    host_aliases => 'db',
+  }
 }
