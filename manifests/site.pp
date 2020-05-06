788f038bbcadf25a09f2321e855023d120b35fd7
Soren8 <chris.wall8@gmail.com>
2020-05-06 19:02:50 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,15 +8,6 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn}",
     owner => 'root',
   }
-  file { '/etc/hosts':
-    ensure => file,
-    owner => 'root',
-    content => '127.0.0.1	master.puppet.vm	master
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
-::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-172.18.0.2	web.puppet.vm	web
-172.18.0.3	db.puppet.vm	db'
-  }
 }
 
 node /^web/ {
